6d74c8c2165e0e40e75a5b21154f96baa1d5ada8
JonnyBoy8 <jtha99@uw.edu>
2023-04-01 15:52:58 -0700

    added bomb scene
    Bomb can now move around, and as additional keybind 'e' to blow up in addition to button

---
+++ bomb_that_destroys_enemies_in_radius/Assets/BombBehavior.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class BombBehavior : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public float speed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        MoveObject();
+    }
+
+    public void DetonateBomb()
+    {
+        GameObject blast = Instantiate(Resources.Load("Prefabs/BlastRadius") as GameObject);
+        Vector3 p = this.transform.position;
+        blast.transform.position = p;
+        Destroy(this.gameObject);
+    }
+
+    void MoveObject()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            DetonateBomb();
+        }
+
+        transform.position = pos;
+    }
+}
